e8a21e5a77781023c5509d15128731c021b793cf
Saurabh Agrawal <agrasaur@umail.iu.edu>
2017-08-26 13:43:55 -0400

    Calling authorization service

--- authentication-service/authentication_Service.py
+++ authentication-service/authentication_Service.py
@@ -3,6 +3,7 @@
 
 from flask import Flask
 from flask_restful import Resource, Api
+import requests
 import mysql.connector
 
 app = Flask(__name__)
@@ -15,17 +16,23 @@ cnx = mysql.connector.connect(user='authentication_user',
 cursor = cnx.cursor()
 
 class Authenticator(Resource):
-    def get(self, id, password):
+    def get(self, id, password, resource):
         global cursor
         query = "SELECT COUNT(*) AS cnt FROM `authentications` WHERE user_id=%s AND user_pwd=%s;"
         cursor.execute(query, [id, password])
         count = cursor.fetchone()[0]
         if count >= 1:
-            return "You are authentic user"
+            host = "localhost"
+            port = 3000
+            url = 'http://%s:%d/id/%s/%s/' %(host, port, id, resource)
+            print "url ->", url
+            r = requests.get(url)
+            return r.text, r.status_code
         else:
-            return "You are not authentic user"
+            bad_credentials = 401
+            return "You are not authentic user", bad_credentials
 
-api.add_resource(Authenticator, '/id/<string:id>/password/<string:password>')
+api.add_resource(Authenticator, '/id/<string:id>/password/<string:password>/resource_id/<string:resource>')
 
 if __name__ == '__main__':
     app.run()
