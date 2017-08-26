c685ec3c908024bc917e514f502df1129c456cee
Saurabh Agrawal <agrasaur@umail.iu.edu>
2017-08-26 16:10:40 -0400

    Eliminating use of flask-restful and only using flask

--- authentication-service/authentication_Service.py
+++ authentication-service/authentication_Service.py
@@ -1,13 +1,11 @@
 # Microservice to authenticate a user
-# Reference: https://flask-restful.readthedocs.io/en/0.3.5/quickstart.html
+# Reference: http://flask.pocoo.org/
 
 from flask import Flask
-from flask_restful import Resource, Api
 import requests
 import mysql.connector
 
 app = Flask(__name__)
-api = Api(app)
 
 cnx = mysql.connector.connect(user='authentication_user',
                               password='password',
@@ -15,24 +13,29 @@ cnx = mysql.connector.connect(user='authentication_user',
                               database='authentication-db')
 cursor = cnx.cursor()
 
-class Authenticator(Resource):
-    def get(self, id, password, resource):
-        global cursor
-        query = "SELECT COUNT(*) AS cnt FROM `authentications` WHERE user_id=%s AND user_pwd=%s;"
-        cursor.execute(query, [id, password])
-        count = cursor.fetchone()[0]
-        if count >= 1:
-            host = "localhost"
-            port = 3000
-            url = 'http://%s:%d/id/%s/%s/' %(host, port, id, resource)
-            print "url ->", url
-            r = requests.get(url)
-            return r.text, r.status_code
-        else:
-            bad_credentials = 401
-            return "You are not authentic user", bad_credentials
-
-api.add_resource(Authenticator, '/id/<string:id>/password/<string:password>/resource_id/<string:resource>')
+@app.route('/id/<string:id>/password/<string:password>/resource_id/<string:resource>', methods=['GET'])
+def authenticate_user(id, password, resource):
+    """This function performs user authentication by checking the DB table 'authentications'
+    and forwards the request to authorization service
+    :param id: User ID
+    :param password: User password
+    :param resource: The requested resource
+    :return: The http response
+    """
+    global cursor
+    query = "SELECT COUNT(*) AS cnt FROM `authentications` WHERE user_id=%s AND user_pwd=%s;"
+    cursor.execute(query, [id, password])
+    count = cursor.fetchone()[0]
+    if count >= 1:
+        host = "localhost"
+        port = 3000
+        url = 'http://%s:%d/id/%s/%s/' % (host, port, id, resource)
+        print "url ->", url
+        r = requests.get(url)
+        return r.text, r.status_code
+    else:
+        bad_credentials = 401
+        return "You are not authentic user", bad_credentials
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5000)
